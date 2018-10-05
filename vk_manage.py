b3d7a98ca067dc6889f28e5ab0d7a5ed7fd91285
nogip <nogip@protonmail.com>
2018-10-06 01:17:31 +0300

    add configuration

--- vk_manage.py
+++ vk_manage.py
@@ -1,25 +1,31 @@
-from commands.BroadcastCommand import BroadcastCommand, AdminBroadcastCommand
-from commands.SkippedLectionsCommand import SkippedLectionsCommand
-from commands.TopicTimetableCommand import TopicTimetableCommand
+from commands.main.BroadcastCommand import BroadcastCommand, AdminBroadcastCommand
+from commands.applied.SkippedLectionsCommand import SkippedLectionsCommand
+from commands.applied.TopicTimetableCommand import TopicTimetableCommand
+from commands.system.HelpCommand import HelpCommand
 
 from core.handlers.ChatHandler import ChatHandler
 from core.observers.CommandObserver import CommandObserver
+from core.settings.ConfigParser import ConfigParser
+
 from database import DatabaseORM
-from database.DatabaseORM import Member
 from database.DBProxy import DBProxy
-from vk_communicate.BaseCommunicateVK import BaseCommunicateVK
-from vk_communicate.BotAccount import BotAccount
-from vk_communicate.group_manager.Group import Group
+
+from vk_communicate.controller.BaseCommunicateVK import BaseCommunicateVK
+from vk_communicate.controller.BotAccount import BotAccount
+from vk_communicate.controller.Group import Group
+
+from settings import *
 
 
 class VKManage:
 
     def __init__(self, token=None, login=None, password=None):
-        self.storage = DBProxy(DatabaseORM)
         self.session = BaseCommunicateVK.create_session(token, login, password)
+
+        self.storage = DBProxy(DatabaseORM)
         self.group = Group(self.session, self.storage).setup().update_members()
         self.chat = ChatHandler(self.group, CommandObserver.get_observer())
-        self.bot = BotAccount.get_account().auth()
+        self.bot = BotAccount.get_account().auth(login, password)
 
     def start(self):
         self.chat.run()
@@ -29,10 +35,11 @@ class VKManage:
 
     def get_command(self, command_name):
         return {
-            "расписание": TopicTimetableCommand().setup_account(self.bot.api),
             "рассылка участникам": BroadcastCommand(),
             "рассылка админам": AdminBroadcastCommand(),
-            "учет прогулов": SkippedLectionsCommand()
+            "помощь": HelpCommand(),
+            "учет прогулов": SkippedLectionsCommand(),
+            "расписание": TopicTimetableCommand().setup_account(self.bot.api),
         }.get(command_name)
 
     def connect_command(self, command_name):
@@ -45,7 +52,10 @@ class VKManage:
         for i in command_names.split(','): self.connect_command(i.strip())
         return self
 
+
 if __name__ == '__main__':
-    VKManage(token="")\
-        .connect_commands("рассылка участникам, рассылка админам, расписание, учет прогулов")\
+    config = ConfigParser(project_path)
+
+    VKManage(token=config['token'], login=config['login'], password=config['password'])\
+        .connect_commands("помощь, рассылка участникам, рассылка админам, учет прогулов")\
         .start()
